7510ec1bed476ea54b0b73dd2622ad1020fa5428
Ramin <ramin@localhost.localdomain>
2015-04-10 19:20:14 +0200

    GCC 2.9 Fix

--- lib/include/webserver_api_functions.h
+++ lib/include/webserver_api_functions.h
@@ -24,39 +24,39 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _WEBSERVER_API_FUNCTIONS_
 #define _WEBSERVER_API_FUNCTIONS_
 
-//#include "webserver.h"
-//#include "simclist.h"
 
 #include "WebserverConfig.h"
 #include "webserver_api_functions_macros.h"
 
-#define NEED_RESUL_CHECK __attribute__((warn_unused_result))
+#if __GNUC__ > 2
+	#define NEED_RESUL_CHECK __attribute__((warn_unused_result))
+#else
+	#define NEED_RESUL_CHECK
+#endif
 
 
-//typedef void WEBSERVER_MUTEX;
 
-
-// Authentifizierung über Cookies
+/* Authentifizierung über Cookies */
 #define NORMAL_CHECK_OK			1
 #define SSL_CHECK_OK			2
 #define NOT_REGISTERED			0
 #define SESSION_MISMATCH_ERROR -1
 
-// Session Status
+/* Session Status */
 #define NOT_REGISTERED 	0
 #define REGISTERED 		1
 
 
-// Session Store Typen
+/* Session Store Typen */
 #define STORE_NORMAL  1
 #define STORE_SSL	  2
 
-// Condition Rückgabewerte
+/* Condition Rückgabewerte */
 #define COND_TRUE 		1
 #define COND_FALSE 		2
 #define COND_ERROR  	3
 
-// Websocket Signale
+/* Websocket Signale */
 #define WEBSOCKET_CONNECT 		1
 #define WEBSOCKET_MSG 			2
 #define WEBSOCKET_DISCONNECT 	3
@@ -95,33 +95,33 @@ void WebserverGenerateGUID(char* buf, int length);
 *                                                                                              *
 ************************************************************************************************/
 
-//
-// 		Variable in den Ausgabe Buffer schreiben
-//
+/*
+ 		Variable in den Ausgabe Buffer schreiben
+*/
 void sendHTMLVariable(dummy_handler* s,dummy_var* var);
 
-//
-//		printf Funktion die in der Ausgabe Buffer schreibt
-//
+/*
+		printf Funktion die in der Ausgabe Buffer schreibt
+*/
 void printHTML(dummy_handler* s,const char *fmt,...) __attribute__ ((format (printf, 2, 3)));
 
-//
-//		Buffer in den Ausgabe Buffer kopieren
-//
+/*
+		Buffer in den Ausgabe Buffer kopieren
+*/
 void sendHTML(dummy_handler* s, const char* text, const unsigned int length);
 
-//
-// 		printf Funktion die in den firelogger Buffer schreibt
-//
-// https://github.com/darwin/firelogger/wiki
-// https://addons.mozilla.org/de/firefox/addon/firelogger/
-//
-#define FireLoggger(...) addFireLogger(s,(char*)__BASE_FILE__, __LINE__, __VA_ARGS__)
+/*
+ 		printf Funktion die in den firelogger Buffer schreibt
+
+ 	https://github.com/darwin/firelogger/wiki
+	 https://addons.mozilla.org/de/firefox/addon/firelogger/
+*/
+#define FireLoggger( ARGS... ) addFireLogger(s,(char*)__BASE_FILE__, __LINE__, __VA_ARGS__)
 void addFireLogger(dummy_handler* s, const char* filename, int fileline, const char* fmt, ... ) __attribute__ ((format (printf, 4, 5)));;
 
-//
-//		Globalen Variablen locken / unlocken
-//
+/*
+		Globalen Variablen locken / unlocken
+*/
 int NEED_RESUL_CHECK lockGlobalVars(void);
 int NEED_RESUL_CHECK unlockGlobalVars(void);
 
@@ -145,43 +145,43 @@ dummy_var* getSessionVarGUID(char* store_guid, int store, const char* name,WS_VA
 
 char* getSessionGUID(dummy_handler* s);
 
-//
-//		Session Variable setzen
-//
-//		store = STORE_NORMAL ] STORE_SSL
-//
+/*
+		Session Variable setzen
+
+		store = STORE_NORMAL ] STORE_SSL
+*/
 char setSessionVar(dummy_handler* s,int store,const char* name,const char* value);
 
-//
-//		Session Variable holen
-//
-//		store = STORE_NORMAL ] STORE_SSL
-//
+/*
+		Session Variable holen
+
+		store = STORE_NORMAL ] STORE_SSL
+*/
 dummy_var* getSessionVar(dummy_handler* s,int store,const char* name,WS_VAR_FLAGS flags);
 
 
-//
-//		Render Variable setzen
-//
+/*
+		Render Variable setzen
+*/
 void setRenderVar   (dummy_handler* s,char* name,char* text);
 void setRenderVar_v (dummy_handler* s, char* name, char* format,...) __attribute__ ((format (printf, 3, 4)));
 
 
-//
-//		Render Variable holen
-//
+/*
+		Render Variable holen
+*/
 dummy_var* getRenderVar(dummy_handler* s,const char* name,WS_VAR_FLAGS flags);
 
 
-//
-//		Glole Variable setzen
-//
+/*
+		Glole Variable setzen
+*/
 dummy_var* setGlobalVar(const char* name,const char* text);
 
 
-//
-//		Glole Variable holen
-//
+/*
+		Glole Variable holen
+*/
 dummy_var* getGlobalVar(const char* name,WS_VAR_FLAGS flags);
 
 
@@ -191,14 +191,14 @@ dummy_var* getGlobalVar(const char* name,WS_VAR_FLAGS flags);
 *                                                                                              *
 ************************************************************************************************/
 
-//
-//		Variable zu einem Array machen
-//
+/*
+		Variable zu einem Array machen
+*/
 void setVariableToArray(dummy_var* var);
 
-//
-//		Neue Variable in einem Array erzeugen
-//
+/*
+		Neue Variable in einem Array erzeugen
+*/
 dummy_var* addToVarArray(dummy_var* var, const char* name);
 dummy_var* addToVarArrayCustomData(dummy_var* var, const char* name,free_handler handle, void* data );
 
@@ -207,32 +207,32 @@ void addToVarArrayStr(dummy_var* var, const char* name, char* buffer );
 
 void* getVariableAsCustomData(dummy_var* var);
 
-//
-//		Variablen Inhalt als String lesen
-//
+/*
+		Variablen Inhalt als String lesen
+*/
 int   getVariableAsString(dummy_var* var, char* buffer,unsigned int buffer_length);
 
-//
-//		Internen Variablen String Pointer lese
-//
-//      ACHTUNG : Pointer nicht speichern da die Variablen vom webserver gelöscht werden können
-//
+/*
+		Internen Variablen String Pointer lese
+
+      ACHTUNG : Pointer nicht speichern da die Variablen vom webserver gelöscht werden können
+*/
 char* getVariableAsStringP(dummy_var* var);
 
-//
-//		Variablen Inhalt als String schreiben
-//
+/*
+		Variablen Inhalt als String schreiben
+*/
 void setVariableAsString(dummy_var* var, const char* text);
 
-//
-//		Variablen Inhalt als Int lesen
-//
+/*
+		Variablen Inhalt als Int lesen
+*/
 int getVariableAsInt(dummy_var* var);
 uint64_t getVariableAsULong(dummy_var* var);
 
-//
-//		Variablen Inhalt als Int schreiben
-//
+/*
+		Variablen Inhalt als Int schreiben
+*/
 void setVariableAsInt(dummy_var* var, int value);
 void setVariableAsULong(dummy_var* var, uint64_t value);
 
@@ -251,18 +251,18 @@ void 		stopArrayIterate(dummy_var* var);
 *                                                                                              *
 ************************************************************************************************/
 
-//
-//		Session Registrierungs Status einstellen
-//
-//		status = REGISTERED  |  NOT_REGISTERED
-//
+/*
+		Session Registrierungs Status einstellen
+
+		status = REGISTERED  |  NOT_REGISTERED
+*/
 char setUserRegisterStatus(dummy_handler* s,char status);
 
-//
-//      Session Register Status abrufen
-//
-//      ret = NORMAL_CHECK_OK | SSL_CHECK_OK | NOT_REGISTERED | SESSION_MISMATCH_ERROR
-//
+/*
+      Session Register Status abrufen
+
+      ret = NORMAL_CHECK_OK | SSL_CHECK_OK | NOT_REGISTERED | SESSION_MISMATCH_ERROR
+*/
 int checkUserRegisterStatus(dummy_handler* s);
 
 /***********************************************************************************************
@@ -284,46 +284,46 @@ char*  getPostData(dummy_handler* s);
 *                                                                                              *
 ************************************************************************************************/
 
-//
-//      URL des Requests
-//
+/*
+      URL des Requests
+*/
 char *getRequestURL(dummy_handler* s);
 
-//
-//      Ob der Request über eine SSL Verbindung kam
-//
+/*
+      Ob der Request über eine SSL Verbindung kam
+*/
 char isRequestSecure(dummy_handler *s);
 
-//
-//      GET Parameter des Requests abfragen
-//
+/*
+      GET Parameter des Requests abfragen
+*/
 dummy_var* getURLParameter(dummy_handler* s,const char* name);
 
-//
-//      Parameter die in der Templateengine mit {f:<name>:para1:para2:...} uebergeben wurden
-//
+/*
+      Parameter die in der Templateengine mit {f:<name>:para1:para2:...} uebergeben wurden
+*/
 char* getEngineParameter(dummy_handler* s,int index);
 void  dumpEngineParameter(dummy_handler* s);
 
-//
-//      Text Frame über einen Websocket senden
-//
+/*
+      Text Frame über einen Websocket senden
+*/
 int WebsocketSendTextFrame(const char* guid, const char* in, const int length);
 int WebsocketSendBinaryFrame(const char* guid, const char* in, const int length);
-//
-//      Close Frame über einen Websocket senden
-//
+/*
+      Close Frame über einen Websocket senden
+*/
 int WebsocketSendCloseFrame(const char* guid);
 
-//
-//      Dir als /<alias>/ im Webserver einblenden
-//
+/*
+      Dir als /<alias>/ im Webserver einblenden
+*/
 void WebserverAddFileDir(const char* alias,const char* dir);
 void WebserverAddFileDirNoCache(const char* alias,const char* dir);
 
-//
-//		Datei <path> als Webserver Plugin laden
-//
+/*
+		Datei <path> als Webserver Plugin laden
+*/
 int WebserverLoadPlugin(const char* name,const char* path);
 
 
@@ -331,13 +331,12 @@ void WebserverAddTemplateFilePostfix(const char* postfix);
 
 void WebserverAddTemplateIgnoreFilePostfix(const char* postfix);
 
-// Urls die nicht im RAM gecached werden soll
+/* Urls die nicht im RAM gecached werden soll */
 void WebserverAddNoRamCacheFile( char* path );
 
 void RegisterEngineFunction(const char* name,user_api_function f,const char* file,int line);
 void RegisterEngineCondition(const char* name,user_api_condition f,const char* file,int line);
 void RegisterWebsocketHandler(const char* url,websocket_api_handler f,const char* file,int line);
-//void RegisterPluginErrorHandler(plugin_error_handler f);
 
 void WebserverRegisterPluginErrorHandler(plugin_error_handler f);
 
@@ -352,11 +351,6 @@ int  WebserverConfigGetInt(const char* name);
 void WebserverConfigSetText(const char* name, const char* text);
 
 void WebserverInjectExternFD(int fd, extern_handler handle );
-
-//void loadPlugin(char* path);
-//int handleWebsocketConnection(WEBSOCKET_SIGNALS signal ,char* guid, char* url,char* msg);
-
-
 
 #ifdef __cplusplus
 }
