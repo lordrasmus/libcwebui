a49ceffaea522e10d7e9abf84248d57050ec6b50
Ramin <ramin@localhost.localdomain>
2015-04-10 19:22:39 +0200

    GCC 2.9 Fix

--- lib/include/intern/cookie.h
+++ lib/include/intern/cookie.h
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #define _COOKIE_H_
 
 #ifdef __GNUC__
-	//#include "../platform/include/platform.h"
 	#include "dataTypes.h"
 #endif
 
