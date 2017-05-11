378aed898b09ed65cebd6590aa9bdf4bbe1720ab
Ramin <lordrasmus@gmail.com>
2017-05-11 22:28:03 +0200

    oss-fuzz update

--- oss_fuzz/main.c
+++ oss_fuzz/main.c
@@ -1,119 +1,36 @@
-//#include <iostream>	
-#include <ifaddrs.h>
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <time.h>
-#include <syslog.h>
-#include <dlfcn.h>
-#include <sys/socket.h>
-#include <netdb.h>
+
+#include <webserver.h>
 
 
-#include "webserver_api_functions.h"
+int main( int argc, char** argv){
 
-void termination_handler(int signum) {
-	WebserverShutdownHandler();
-}
+	char buf[10000];
 
-void sig_pipe_hanler(int signum) {
-	//printf("Sig Pipe\n");
-}
+	#ifdef AFL_PER
+	while (__AFL_LOOP(10000)) {
+	#endif
+		memset(buf, 0, 10000);
+		int len = read(0, buf, 10000);
 
-int my_cors_handler( cors_infos* infos){
-	printf("CORS Handler: %s\n",ws_get_cors_type_name( infos->type ));
-	switch( infos->type){
-		case CORS_ALLOW_ORIGIN:
-			printf("  Origin: %s\n",infos->origin);
-			if ( 0 == strcmp( infos->origin, "www.google.de" ) ){
-				return COND_TRUE;
-			}
-			
-			break;
-		case CORS_ALLOW_METHODS:
-			printf("  Method: %s\n",infos->method);
-			if ( 0 == strcmp( infos->method, "GET" ) ){
-				return COND_TRUE;
-			}
-			if ( 0 == strcmp( infos->method, "POST" ) ){
-				return COND_TRUE;
-			}
-			if ( 0 == strcmp( infos->method, "OPTIONS" ) ){
-				return COND_TRUE;
-			}
-			break;
-		
-		case CORS_ALLOW_CREDENTIALS:
-			//return COND_FALSE;
-			printf("  Allow Credential\n");
-			return COND_TRUE;
-		
-		case CORS_ALLOW_HEADERS:
-			printf("  Allow Headers\n");
-			return COND_TRUE;
+
+		socket_info sock;
+		HttpRequestHeader *header = WebserverMallocHttpRequestHeader();
+
+		memset(&sock , 0, sizeof( socket_info ));
+
+		sock.header = header;
+
+		unsigned int bytes_parsed;
+		ParseHeader( &sock, header, buf, len , &bytes_parsed );
+
+		WebserverFreeHttpRequestHeader( header );
+
+	#ifdef AFL_PER
 	}
-	
-	return COND_FALSE;
-}
+	#endif
 
-
-extern unsigned char data__[];
-
-int main(int argc, char **argv) {
-
-
-	if (signal(SIGINT, termination_handler) == SIG_IGN )  signal(SIGINT, SIG_IGN);
-	if (signal(SIGHUP, termination_handler) == SIG_IGN )  signal(SIGHUP, SIG_IGN);
-	if (signal(SIGTERM, termination_handler) == SIG_IGN ) signal(SIGTERM, SIG_IGN);
-	if (signal(SIGPIPE, sig_pipe_hanler) == SIG_IGN	)     signal(SIGPIPE, SIG_IGN);
-
-	openlog("webserver", LOG_CONS, LOG_LOCAL0);
-
-	if (0 == WebserverInit()) {
-
-		//WebserverAddBinaryData( data__ );
-
-		ws_set_cors_handler( my_cors_handler );
-
-		#ifdef WEBSERVER_USE_PYTHON
-
-		WebserverInitPython();
-		WebserverLoadPyPlugin( "../testSite/test.py" );
-		WebserverLoadPyPlugin( "../testSite/test2.py" );
-
-		WebserverConfigSetInt( "reload_py_modules",1);
-
-		#endif
-
-		WebserverConfigSetInt( "use_csp",0);
-
-		WebserverAddFileDir("", "../testSite/www");
-		WebserverAddFileDir("img", "../testSite/img");
-
-
-		WebserverLoadPlugin("TestPlugin", "../testSite/src/test_plugin.so");
-
-		WebserverConfigSetInt("port",8080);
-		WebserverConfigSetInt("ssl_port",4443);
-		WebserverConfigSetText("ssl_file_path", "./");
-		WebserverConfigSetText("ssl_key_file", "server.pem");
-		WebserverConfigSetText("ssl_key_file_password", "password");
-		WebserverConfigSetText("ssl_dh_file", "dh1024.pem");
-		WebserverConfigSetText("ssl_ca_list_file", "root.pem");
-
-
-		WebserverStart();
-	}
-
-	WebserverShutdown();
 
 	return 0;
 }
-
-
-
