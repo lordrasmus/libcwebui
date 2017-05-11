c3cc98266f731adfe27bf8d0815ceeaef6eeebd5
Ramin <lordrasmus@gmail.com>
2017-05-12 00:38:50 +0200

    oss-fuzz update

--- oss_fuzz/main.c
+++ oss_fuzz/main.c
@@ -13,16 +13,20 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
 	socket_info sock;
 	HttpRequestHeader *header = WebserverMallocHttpRequestHeader();
+	
+	char *buffer = malloc( Size );
+	memcpy( buffer, Data, Size ); // Input data is modified
 
 	memset(&sock , 0, sizeof( socket_info ));
 
 	sock.header = header;
 
 	unsigned int bytes_parsed;
-	ParseHeader( &sock, header, (char*)Data, Size , &bytes_parsed );
+	ParseHeader( &sock, header, buffer, Size , &bytes_parsed );
 
 	WebserverFreeHttpRequestHeader( header );
 
+	free( buffer );
 
 	return 0;
 }
