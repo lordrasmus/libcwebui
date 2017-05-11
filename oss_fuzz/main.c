5d7023c2949b926215aafd43ab8f1452ba1316ce
Ramin <lordrasmus@gmail.com>
2017-05-12 00:26:19 +0200

    oss-fuzz update

--- oss_fuzz/main.c
+++ oss_fuzz/main.c
@@ -3,6 +3,12 @@
 
 #include <webserver.h>
 
+extern "C" void deleteEvent(socket_info* sock){}
+extern "C" unsigned long dumpSocketsSize(int *count) { return *count; }
+extern "C" void dumpSockets(http_request* s) { }
+extern "C" char isTemplateFile(const char* file) { return 0; }
+
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
 	socket_info sock;
