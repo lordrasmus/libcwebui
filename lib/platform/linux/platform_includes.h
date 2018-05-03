1c24f1c2ea90b1d68c014c601da747167ae807c9
lordrrasmus <lordrasmus@gmail.com>
2018-05-03 21:45:10 +0200

    linux updates

--- lib/platform/linux/platform_includes.h
+++ lib/platform/linux/platform_includes.h
@@ -2,12 +2,20 @@
 #ifndef _PLATFORM_INCLUDES_H_
 #define _PLATFORM_INCLUDES_H_
 
+#ifdef LINUX
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
 #include <dlfcn.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/sendfile.h>
+
+#include <strings.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <netinet/in.h>
 
@@ -15,11 +23,14 @@
 #include <sys/ioctl.h>
 
 
+#define FILE_OFFSET __off_t
 
-#define WS_MUTEX_TYPE pthread_mutex_t
-#define WS_SEMAPHORE_TYPE sem_t
+#define WS_MUTEX_TYPE		pthread_mutex_t
+#define WS_SEMAPHORE_TYPE	sem_t
+#define WS_THREAD			pthread_t
 
 #define WebServerPrintf printf
 
+#endif
 
 #endif
