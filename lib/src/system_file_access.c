3713912631d69359973636551a5c7a6b95740f01
lordrrasmus <lordrasmus@gmail.com>
2018-05-03 21:58:07 +0200

    etag generation of ram cached files update

--- lib/src/system_file_access.c
+++ lib/src/system_file_access.c
@@ -256,12 +256,11 @@ WebserverFileInfo VISIBLE_ATTR *getFile( char *name)  {
 	}
 
 
+	ramCacheFile(file);
 
 #ifndef WEBSERVER_DISABLE_CACHE
-	generateEtag ( file );
+	generateEtag(file);
 #endif
-
-	ramCacheFile(file);
 
 
 	#ifdef WEBSERVER_USE_WNFS
