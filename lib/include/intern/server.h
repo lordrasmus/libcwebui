e3c0db8c757c421e7a5de2f529d06fbc10e1e5b6
Ramin Moussavi <lordrasmus@gmail.com>
2019-03-31 11:34:00 +0200

    add missing include

--- lib/include/intern/server.h
+++ lib/include/intern/server.h
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _WEBSERVER_SERVER_H_
 #define _WEBSERVER_SERVER_H_
 
+#include <stdarg.h>
 #include <webserver.h>
 
 
