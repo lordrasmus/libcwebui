9b1463553c049f1318fef17adb3d905d9af78a44
Ramin Moussavi <lordrasmus@gmail.com>
2019-04-21 20:42:33 +0200

    Barrelfish test started

---
+++ Samples/Barrelfish/main.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello Webserver\n");
+	return 0;
+}
---
+++ lib/platform/barrelfish/platform_includes.h
@@ -0,0 +1,19 @@
+
+#ifndef _PLATFORM_INCLUDES_H_
+#define _PLATFORM_INCLUDES_H_
+
+#ifdef BARRELFISH
+
+#define FILE_OFFSET int
+
+#define WS_MUTEX_TYPE int
+#define WS_SEMAPHORE_TYPE int
+
+#define TIME_TYPE int
+#define SIZE_TYPE size_t
+
+
+#define INET_ADDRSTRLEN 15
+
+#endif
+#endif
