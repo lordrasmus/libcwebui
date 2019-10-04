2b24686c980a80e3493f67adc2201d22c3217b00
Ramin Moussavi <lordrasmus@gmail.com>
2019-10-04 05:08:38 -0700

    OSX Test

--- Samples/OSX/MakeProject/check_installation.sh
+++ Samples/OSX/MakeProject/check_installation.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
-which brew
+which brew > /dev/null
 
 if [ ! 0 -eq $? ] ; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "#include <event2/event.h>" > /tmp/test.c
+clang -c -o /tmp/test.out /tmp/test.c > /dev/null
+
+if [ ! 0 -eq $? ] ; then
+	brew install libevent
 fi
