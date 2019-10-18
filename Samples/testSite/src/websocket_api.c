34430d8cc59e4a1d8219f5e6fba2f15497edd290
lordrrasmus <lordrasmus@gmail.com>
2019-10-18 14:22:17 +0200

    make testSite usable in win32 sample

--- Samples/testSite/src/websocket_api.c
+++ Samples/testSite/src/websocket_api.c
@@ -1,12 +1,24 @@
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <windows.h>
+
+#endif
+
 #undef _POSIX_SOURCE
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
-#include <unistd.h>
+
+#ifdef __linux__
+	#include <unistd.h>
+	#include <pthread.h>
+#endif
+
+
+
 #include <time.h>
 
 #include "webserver_api_functions.h"
@@ -14,6 +26,46 @@
 
 
 #ifdef WEBSERVER_USE_WEBSOCKETS
+
+typedef void*(*thread_function)(void*);
+
+#ifdef _MSC_VER
+
+#define pthread_mutex_t HANDLE 
+
+
+
+void run_thread(thread_function func , void* arg) {
+
+	DWORD   dwThreadIdArray;
+
+	CreateThread(
+		NULL,                   // default security attributes
+		0,                      // use default stack size  
+		func,       // thread function name
+		arg,          // argument to thread function 
+		0,                      // use default creation flags 
+		&dwThreadIdArray);   // returns the thread identifier 
+}
+
+int pthread_mutex_lock(pthread_mutex_t* mutex) {
+
+	WaitForSingleObject( mutex, INFINITE);
+	return 0;
+
+}
+
+void pthread_mutex_unlock(pthread_mutex_t* mutex) {
+
+	ReleaseMutex(mutex);
+
+}
+
+void pthread_mutex_init(pthread_mutex_t* mutex, int init) {
+	*mutex = CreateMutex(0, FALSE, 0);
+}
+
+#endif
 
 
 #include <linked_list.h>
@@ -34,11 +86,17 @@ static int getTime(char* buffer) {
 	return sprintf(buffer, "%02d:%02d:%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 }
 
-static void mysleep(unsigned long ticks) {
-	struct timespec req;
+static void mysleep(unsigned long seconds) {
+	/*struct timespec req;
 	req.tv_sec = 0;
 	req.tv_nsec = 100000000;
-	nanosleep(&req, 0);
+	nanosleep(&req, 0);*/
+#ifdef _MSC_VER
+	Sleep(seconds * 1000);
+#else
+	sleep(seconds);
+#endif
+
 }
 
 /*******************************************************************
@@ -48,6 +106,7 @@ static void mysleep(unsigned long ticks) {
  *******************************************************************/
 
 list_t clock_clients;
+
 
 pthread_mutex_t clock_mutex;
 
@@ -103,7 +162,7 @@ static void uhr_loop( void ) {
 
 		pthread_mutex_unlock(&clock_mutex);
 
-		mysleep(1000);
+		mysleep(1);
 	}
 }
 
@@ -179,7 +238,7 @@ static void simpleThread(void* p) {
 			WebserverFree(guid);
 			return;
 		}
-		mysleep(1000);
+		mysleep(1);
 	}
 	
 
@@ -195,22 +254,23 @@ static void *simple_function(void *ptr) {
 }
 
 DEFINE_WEBSOCKET_HANDLER( "simple" , simple_handler ) {
-	pthread_t thread1;
+	unsigned long l;
 	char *tmp;
 	switch (signal) {
 	case WEBSOCKET_CONNECT:
 		printf("Websocket API Connect    : %s simple\n", guid);
-		tmp = WebserverMalloc(strlen(guid)+1);
+		l = strlen(guid) + 1;
+		tmp = WebserverMalloc(l);
 		strcpy(tmp, guid);
-		pthread_create(&thread1, NULL, simple_function, tmp);
+		run_thread( simple_function, tmp );
 		break;
 	}
 }
 
 DEFINE_WEBSOCKET_HANDLER( "CommandSocket" , CommandSocket_handler) {
 	
-	char *tmp;
-	tmp = WebserverMalloc(strlen(guid)+1);
+	unsigned long l = strlen(guid) + 1;
+	char *tmp  = WebserverMalloc( l );
 	strcpy(tmp, guid);
 	
 	//printf("command socket : %d\n",signal);
@@ -240,41 +300,10 @@ void startApiThreads( void ) {
 
 	ws_list_init(&clock_clients);
 	
-	pthread_t thread1;
 	pthread_mutex_init(&clock_mutex, 0);
-	pthread_create(&thread1, NULL, clock_start_helper, 0);
+	run_thread( clock_start_helper, 0 );
 
 }
-
-#ifdef _MSC_VER
-
-
-
-//HANDLE uhr_clients_mutex;
-
-DWORD WINAPI UhrThread ( LPVOID Data ) {
-uhr_loop();
-return 0;
-}
-
-DWORD WINAPI ThreadFunc ( LPVOID Data ) {
-simpleThread(Data);
-return 0;
-}
-
-DWORD WINAPI MultiThread ( LPVOID Data ) {
-multiThread(Data);
-return 0;
-}
-
 
 
 #endif
-
-#else // WEBSERVER_USE_WEBSOCKETS
-
-void startApiThreads( void ) {
-	// Dummy
-}
-
-#endif // WEBSERVER_USE_WEBSOCKETS
